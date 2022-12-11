a981bce5fecae8f4d5a2ced4c357b5cae0a45e87
Ricachi <nicolasricciardi2002@gmail.com>
2022-12-11 22:37:12 -0100

    TestUnit

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -1,5 +1,9 @@
 
 package proyecto;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Graphics2D;
+import java.awt.Shape;
 
 
 public class Proyecto {
@@ -7,5 +11,28 @@ public class Proyecto {
         int numero = (int) (Math.random()*1000);
         String nombre = "Pizarra_"+Integer.toString(numero);
         new Paint(nombre);
+        Point q1 = new Point();
+        Point q2 = new Point();
+        
+        Panel p1 = new Panel();
+        
+        Graphics g = null;
+        Graphics2D g2 = null;
+        p1.setG2d(g2);
+        p1.getG2d();
+        p1.paint(g);
+        
+        p1.crearGraphics2D();
+        p1.getFigura();
+        p1.guardarAutomatico(nombre);
+        p1.guardar();
+        p1.abrir();
+        p1.resetAll();
+        p1.setp1(q1);
+        p1.setp2(q2);
+        p1.crearLinea(p1.getp1(), p1.getp2());
+        p1.crearRectangulo(p1.getp1(), p1.getp2());
+        p1.setFigura(p1.crearRectangulo(q1, q2));
+        
     }
 }
